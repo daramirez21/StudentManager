46da1aa950c3ca490574e567419ae7adedf31a9d
daramirez21 <daramirez@mail.csuchico.edu>
2019-12-04 17:51:13 -0800

    add in

---
+++ student.h
@@ -0,0 +1,19 @@
+#ifndef _STUDENT_H_
+#define _STUDENT_H_ 
+
+#include <string>
+
+using namespace std;
+
+class student
+{
+        private:
+        string first;
+        string last;
+        
+        public: 
+        void setName(string, string);
+        string fullName();
+};
+
+#endif
